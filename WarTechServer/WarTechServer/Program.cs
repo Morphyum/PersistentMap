b0bddf9715ec286d224ac0d5899a7a8d7f5597d5
Morphyum <zion@c-schwabe.de>
2018-08-19 16:49:06 +0200

    *some error handling

--- WarTechServer/WarTechServer/Program.cs
+++ WarTechServer/WarTechServer/Program.cs
@@ -9,11 +9,16 @@ using WarTechAPI;
 namespace WarTechServer {
     class Program {
         static void Main(string[] args) {
-            WarServices warServices = new WarServices();
-            WebServiceHost _serviceHost = new WebServiceHost(warServices, new Uri("http://localhost:8000/warServices"));
-            _serviceHost.Open();
-            Console.ReadKey();
-            _serviceHost.Close();
+            try {
+                WarServices warServices = new WarServices();
+                WebServiceHost _serviceHost = new WebServiceHost(warServices, new Uri("http://localhost:8000/warServices"));
+                _serviceHost.Open();
+                Console.WriteLine("Open");
+                Console.ReadKey();
+                _serviceHost.Close();
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
         }
     }
 }
