c05125db414f34e05998fc1e7c67a3ba1f7c7b9c
Gerrit Kitts <gkitts@gmail.com>
2018-10-21 00:43:18 -0400

    Improvements to the HeartBeatMonitor.

--- PersistentMapServer/PersistentMapServer/HeartBeatMonitor.cs
+++ PersistentMapServer/PersistentMapServer/HeartBeatMonitor.cs
@@ -1,6 +1,5 @@
 ﻿using PersistentMapAPI;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
@@ -14,13 +13,16 @@ namespace PersistentMapServer {
         private static string InstanceName_WarServices = "WarServices@" + Program.ServiceUrl.Replace("/", "|");
 
         private static TimeSpan reportingTimeSpan = TimeSpan.FromSeconds(5);
- 
+
         private static DateTime lastReportedTime = DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(10));
 
-        private static PerformanceCounter pc_calls = null;
-        private static PerformanceCounter pc_callsOutstanding = null;
-        private static PerformanceCounter pc_callsFaulted = null;
-        private static PerformanceCounter pc_callsFailed = null;
+        private static PerformanceCounter pc_sms_calls = null;
+        private static PerformanceCounter pc_sms_callsOutstanding = null;
+        private static PerformanceCounter pc_sms_callsFaulted = null;
+        private static PerformanceCounter pc_sms_callsFailed = null;
+        private static PerformanceCounter pc_sms_pctMaxSessions = null;
+        private static PerformanceCounter pc_sms_pctMaxInstances = null;
+        private static PerformanceCounter pc_sms_pctMaxCalls = null;
 
         public static void DoWork(object sender, DoWorkEventArgs e) {
             BackgroundWorker bw = sender as BackgroundWorker;
@@ -32,8 +34,12 @@ namespace PersistentMapServer {
                 // If more than reportTimeSpan was passed since we last logged values, log them
                 DateTime now = DateTime.UtcNow;
                 if (now.Subtract(reportingTimeSpan) > lastReportedTime) {
-                    Logger.LogLine("Reporting values!");
-                    Logger.LogLine($"Calls - Total: ({pc_calls.NextValue()}) Outstanding: ({pc_callsOutstanding.NextValue()}) Faulted: ({pc_callsFaulted.NextValue()}) Failed: ({pc_callsFailed.NextValue()})");
+                    Logger.LogLine("-------- HeartBeat --------");
+                    Logger.LogLine($"  Calls - Total:({pc_sms_calls.NextValue()}) Outstanding:({pc_sms_callsOutstanding.NextValue()}) " + 
+                        $"Faulted:({pc_sms_callsFaulted.NextValue()}) Failed:({pc_sms_callsFailed.NextValue()}) " +
+                        $" Max% ({pc_sms_pctMaxCalls.NextValue()})%"
+                        );
+                    
                     lastReportedTime = now;
                 }
 
@@ -43,18 +49,19 @@ namespace PersistentMapServer {
         }
 
         private static void initializePerformanceCounters() {
-            
-            HeartBeatMonitor.pc_calls =
+            HeartBeatMonitor.pc_sms_calls =
                 new PerformanceCounter(CategoryName_ServiceModelService, "calls", InstanceName_WarServices);
-            HeartBeatMonitor.pc_callsOutstanding =
+            HeartBeatMonitor.pc_sms_callsOutstanding =
                 new PerformanceCounter(CategoryName_ServiceModelService, "calls outstanding", InstanceName_WarServices);
-            HeartBeatMonitor.pc_callsFaulted =
+            HeartBeatMonitor.pc_sms_callsFaulted =
                 new PerformanceCounter(CategoryName_ServiceModelService, "calls faulted", InstanceName_WarServices);
-            HeartBeatMonitor.pc_callsFailed =
+            HeartBeatMonitor.pc_sms_callsFailed =
                 new PerformanceCounter(CategoryName_ServiceModelService, "calls failed", InstanceName_WarServices);
+            HeartBeatMonitor.pc_sms_pctMaxCalls =
+                new PerformanceCounter(CategoryName_ServiceModelService, "percent of max concurrent sessions", InstanceName_WarServices);
         }
-
-        private void printCategoryNames() {
+    
+        private static void printCategoryNames() {
             PerformanceCounterCategory[] perfCategories = PerformanceCounterCategory.GetCategories();
             List<PerformanceCounterCategory> categories = new List<PerformanceCounterCategory>(perfCategories);
             var categoryNames = categories.ConvertAll(new Converter<PerformanceCounterCategory, String>(CategoryToNameConverter));
