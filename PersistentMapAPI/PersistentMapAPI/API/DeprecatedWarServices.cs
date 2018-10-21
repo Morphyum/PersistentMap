4aab607ffd53d14d0b766eedd201f313b060d5e8
Gerrit Kitts <gkitts@gmail.com>
2018-10-21 11:32:29 -0400

    Adding debug logging for deprecated method invocation.

--- PersistentMapAPI/PersistentMapAPI/API/DeprecatedWarServices.cs
+++ PersistentMapAPI/PersistentMapAPI/API/DeprecatedWarServices.cs
@@ -19,6 +19,7 @@ namespace PersistentMapAPI.API {
         public abstract string PostSalvageForFaction(List<ShopDefItem> salvage, string Faction);
 
         public System PostMissionResultDeprecated(string employer, string target, string systemName, string mresult) {
+            Logger.Debug("WARNING: Deprecated method invoked: PostMissionResultDeprecated");
             try {
                 return PostMissionResult(new MissionResult((Faction)Enum.Parse(typeof(Faction), employer), (Faction)Enum.Parse(typeof(Faction), target), (BattleTech.MissionResult)Enum.Parse(typeof(BattleTech.MissionResult), mresult), systemName, 5, 0, 0), "UNKNOWN");
 
@@ -29,6 +30,7 @@ namespace PersistentMapAPI.API {
         }
 
         public System PostMissionResultDeprecated2(string employer, string target, string systemName, string mresult, string difficulty) {
+            Logger.Debug("WARNING: Deprecated method invoked: PostMissionResultDeprecated2");
             try {
                 return PostMissionResult(new MissionResult((Faction)Enum.Parse(typeof(Faction), employer), (Faction)Enum.Parse(typeof(Faction), target), (BattleTech.MissionResult)Enum.Parse(typeof(BattleTech.MissionResult), mresult), systemName, int.Parse(difficulty), 0, 0), "UNKNOWN");
 
@@ -39,6 +41,7 @@ namespace PersistentMapAPI.API {
         }
 
         public System PostMissionResultDeprecated3(string employer, string target, string systemName, string mresult, string difficulty, string rep) {
+            Logger.Debug("WARNING: Deprecated method invoked: PostMissionResultDeprecated3");
             try {
                 return PostMissionResult(new MissionResult((Faction)Enum.Parse(typeof(Faction), employer), (Faction)Enum.Parse(typeof(Faction), target), (BattleTech.MissionResult)Enum.Parse(typeof(BattleTech.MissionResult), mresult), systemName, int.Parse(difficulty), int.Parse(rep), 0), "UNKNOWN");
 
@@ -49,6 +52,7 @@ namespace PersistentMapAPI.API {
         }
 
         public System PostMissionResultDepricated4(string employer, string target, string systemName, string mresult, string difficulty, string rep, string planetSupport) {
+            Logger.Debug("WARNING: Deprecated method invoked: PostMissionResultDepricated4");
             try {
                 return PostMissionResult(new MissionResult((Faction)Enum.Parse(typeof(Faction), employer), (Faction)Enum.Parse(typeof(Faction), target), (BattleTech.MissionResult)Enum.Parse(typeof(BattleTech.MissionResult), mresult), systemName, int.Parse(difficulty), int.Parse(rep), int.Parse(planetSupport)), "UNKNOWN");
             } catch (Exception e) {
@@ -58,10 +62,12 @@ namespace PersistentMapAPI.API {
         }
 
         public System PostMissionResultDepricated5(MissionResult mresult) {
+            Logger.Debug("WARNING: Deprecated method invoked: PostMissionResultDepricated5");
             return PostMissionResult(mresult, "UNKNOWN");
         }
 
         public string PostPurchaseForFactionDepricated(string Faction, string ID) {
+            Logger.Debug("WARNING: Deprecated method invoked: PostPurchaseForFactionDepricated");
             Faction realFaction = (Faction)Enum.Parse(typeof(Faction), Faction);
             if (Holder.factionShops != null) {
                 FactionShop shop = Holder.factionShops.FirstOrDefault(x => x.shopOwner == realFaction);
