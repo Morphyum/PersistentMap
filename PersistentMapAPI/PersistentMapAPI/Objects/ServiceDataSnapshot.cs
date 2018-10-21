557f07d3203c23d08f32f6074b9790db28c1a982
Gerrit Kitts <gkitts@gmail.com>
2018-10-21 12:05:35 -0400

    Adding result history to service snapshot.

--- PersistentMapAPI/PersistentMapAPI/Objects/ServiceDataSnapshot.cs
+++ PersistentMapAPI/PersistentMapAPI/Objects/ServiceDataSnapshot.cs
@@ -11,7 +11,9 @@ namespace PersistentMapAPI.Objects {
         public int num_connections_inactive = 0;
         public float percent_connections_active = 0.0f;
             
-        public int size_resultHistory = 0;
+        public int num_results = 0;
+        public int num_results_past_inactive_time = 0;
+
         public int size_factionInventory = 0;
         public int size_factionShopsInventory = 0;
 
@@ -27,7 +29,10 @@ namespace PersistentMapAPI.Objects {
             percent_connections_active = size_connectionStore == 0 ? 
                 0.0f : (size_connectionStore - num_connections_inactive) / (size_connectionStore);
 
-            size_resultHistory = Holder.resultHistory.Count;
+            num_results = Holder.resultHistory.Count;
+            num_results_past_inactive_time = Holder.resultHistory
+                .Where(x => x.date < activeOnOrAfter)
+                .Count();
 
         }
     }
