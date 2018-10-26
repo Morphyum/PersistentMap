7433893ad2341049af51df92f84861d3ef9b29d5
Gerrit Kitts <gkitts@gmail.com>
2018-10-25 21:02:29 -0400

    Adding Unit tests for client functionality. Runs against a local instance.

---
+++ PersistentMapClient/PersistentMapClientTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PersistentMapClientTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PersistentMapClientTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("05f4d9c0-1cbb-4d77-adbf-8e9f2c22f30d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PersistentMapClient/PersistentMapClientTests/StarMapTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersistentMapClient;
+
+namespace PersistentMapClientTests {
+
+    [TestClass]
+    public class StarMapTests {
+        [TestMethod]
+        [Timeout(2000)]
+        public void TestGetStarmap() {
+            PersistentMapClient.PersistentMapClient.Init(".", "test.settings.json");
+            ParseMap parsedMap = Web.GetStarMap();
+            Assert.IsNotNull(parsedMap);
+            Assert.AreEqual(parsedMap.systems.Count, 3359);
+        }
+    }
+}
