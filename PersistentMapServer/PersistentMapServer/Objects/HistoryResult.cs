1139123e211e18451760079d381ed5e5096635c2
Gerrit Kitts <gkitts@gmail.com>
2018-11-07 14:46:47 -0500

    Moving API classes to server, as they aren't used on the client.

--- PersistentMapServer/PersistentMapServer/Objects/HistoryResult.cs
+++ PersistentMapServer/PersistentMapServer/Objects/HistoryResult.cs
@@ -3,7 +3,7 @@ using System;
 using System.Globalization;
 using System.Runtime.Serialization;
 
-namespace PersistentMapAPI {
+namespace PersistentMapAPI.Objects {
     public class HistoryResult {
         public DateTime? date;
         public string DateString;
